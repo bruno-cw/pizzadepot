82ec1521a89024796ecda615a000748bf159016b
pizzaguy <pizzaguy@pizzadepot.com>
2015-10-15 09:08:23 -0300

    share

---
+++ Hibernate/src/client/Main.java
@@ -0,0 +1,18 @@
+package client;
+import org.hibernate.Session;
+import util.HibernateUtil;
+import entity.Message;
+
+public class Main {
+    public static void main(String[] args) {        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        Message message = new Message( "Hello Hibernate 5" );
+
+        session.save(message);    
+
+        session.getTransaction().commit();
+        session.close();    
+    }
+}

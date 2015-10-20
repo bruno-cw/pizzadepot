5030a672d31b3d041ff603ef5dbff158814dd2ff
pizzaguy <pizzaguy@pizzadepot.com>
2015-10-20 11:00:43 -0200

    Added metadata bootstrapping

--- Hibernate/src/util/HibernateUtil.java
+++ Hibernate/src/util/HibernateUtil.java
@@ -1,18 +1,25 @@
 package util;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-
-import entity.Message;
 
 public class HibernateUtil {    
     private static final SessionFactory sessionFactory = buildSessionFactory();    
     private static SessionFactory buildSessionFactory() {
         try {           
-            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");  
-            configuration.addAnnotatedClass(Message.class);
-            
-            return configuration.buildSessionFactory( new StandardServiceRegistryBuilder().applySettings( configuration.getProperties() ).build() );
+        	StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+            .configure( "hibernate.cfg.xml" )
+            .build();
+        	
+        	Metadata metadata = new MetadataSources( standardRegistry )
+            .getMetadataBuilder()
+            .build();
+        	
+        	return metadata.getSessionFactoryBuilder().build();
+
         }
         catch (Throwable ex) {                
             throw new ExceptionInInitializerError(ex);

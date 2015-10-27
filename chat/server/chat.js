2647cef8fe6daa13402901a759c5b2590af437f3
pizzaguy <pizaguy@pizzadepot.com>
2015-10-27 08:52:08 -0200

    fixed resource leak, messages is now a list instead of a string

--- chat/server/chat.js
+++ chat/server/chat.js
@@ -1,38 +1,33 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var os = require('os');
+var ip = require('./ipfier.js')
 var replaceStream = require('replacestream')
-var ip = '';
-var messages = '[' + new Date().toString() +']' + '[server]Hello World \n';
 
-Object.keys(os.networkInterfaces()).forEach(function (ifname) {
-	var alias = 0;
 
-	os.networkInterfaces()[ifname].forEach(function (iface) {
-		if ('IPv4' !== iface.family || iface.internal !== false) {
-			return;
-		}
-		console.log(ifname, iface.address);
-		ip = iface.address;
-		++alias;
-	});
-});
+var clients = [];
+var messages = ['[server]Hello World \n']; 
+
+console.log(ip.getIp(),process.argv[2]);
 
 http.createServer(function (request,response) {
-
+	
 	var query = url.parse(request.url,true)
 	if (query.pathname == '/chat/messages' && request.method == 'PUT'){
-		messages = messages + '[' + new Date().toString() +']' + '[' + query.query.id +']'+ query.query.send + '\n'
-
+		messages.push('[' + query.query.id +']'+ query.query.send + '\n')
+		response.writeHead(200);
+		response.end();
+		
 	}else if (query.pathname == '/chat/messages'){		
-		response.writeHead(200, { 'Content-Type': 'application/json' })
-		response.end(JSON.stringify({'messages' : messages}))
+		response.writeHead(200, { 'Content-Type': 'application/json' });
+		response.end(JSON.stringify({'messages' : messages}));
 	}
 	else if (query.pathname == '/chat'){
-		response.writeHead(200, { 'Content-Type': 'text/html' })
+		response.writeHead(200, { 'Content-Type': 'text/html' });
 
-		fs.createReadStream('..\\client\\chat.htm' ).pipe(replaceStream('{IP_ADDRESS}:{PORT}', ip+':'+process.argv[2])).pipe(response);
+		fs.createReadStream('../client/chat.htm' )
+		.pipe(replaceStream('{IP_ADDRESS}:{PORT}', ip.getIp() +':'+process.argv[2]))
+		.pipe(response);
 	}
 
 }).listen(process.argv[2])
---
+++ chat/server/ipfier.js
@@ -0,0 +1,21 @@
+var os = require('os');
+
+ipfier = exports;
+
+ipfier.getIp = function getIp() { 
+	ip = ''
+	
+	Object.keys(os.networkInterfaces()).forEach(function (ifname) {
+	var alias = 0;
+
+	os.networkInterfaces()[ifname].forEach(function (iface) {
+		if ('IPv4' !== iface.family || iface.internal !== false) {
+			return;
+		}
+		ip = iface.address;
+		++alias;
+	});
+	
+});
+	return ip;
+}
